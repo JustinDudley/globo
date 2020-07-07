c3c2181c7623f3b43f5562f2da81b61ed19ab416
justindudley <justin.dudley@ascendum.com>
2020-07-07 15:50:50 -0400

    have Home.js component with View

---
+++ app/views/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Stylesheet, Text, View } from "react-native";
+
+export class Home extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Yot, this will be the Homepage</Text>
+        <Text>Okay, so these other lines are here</Text>
+        <Text>just and...... so you can see the text in the app</Text>
+      </View>
+    );
+  }
+}

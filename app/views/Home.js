0b7ad1953309e485f8ad8dd007b802cc3a0424ce
justindudley <justin.dudley@ascendum.com>
2020-07-08 14:42:49 -0400

    add StyleSheet to Header

--- app/views/Home.js
+++ app/views/Home.js
@@ -7,10 +7,10 @@ export class Home extends React.Component {
   render() {
     return (
       <View>
+        <Header message="Press to Login" />
         <Text>Yot, this will be the Homepage</Text>
         <Text>Okay, so these other lines are here</Text>
         <Text>just and...... so you can see the text in the app</Text>
-        <Header message="Press to Login" />
       </View>
     );
   }
